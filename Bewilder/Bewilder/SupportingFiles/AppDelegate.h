077c78bf6a1fa4ff224c5f6b6f65b0ca8af4cad3
ShenYj <shenyanjie123@foxmail.com>
2017-03-02 20:58:55 +0800

    MVC目录搭建

---
+++ Bewilder/Bewilder/SupportingFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bewilder
+//
+//  Created by ShenYj on 2017/3/2.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
