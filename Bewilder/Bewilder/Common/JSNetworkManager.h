6c2e96e4b14d21b89269892c62a4644cca58a768
ShenYj <shenyanjie123@foxmail.com>
2017-03-10 23:56:20 +0800

    NetworkManager Tool public requesst method

--- Bewilder/Bewilder/Common/JSNetworkManager.h
+++ Bewilder/Bewilder/Common/JSNetworkManager.h
@@ -7,8 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AFHTTPSessionManager.h"
 
+typedef NS_ENUM(NSUInteger, RequestMethod) {
+    RequestMethodGet,
+    RequestMethodPost
+};
 
-@interface JSNetworkManager : NSObject
+@interface JSNetworkManager : AFHTTPSessionManager
+
+/** 单例 */
++ (instancetype)sharedManager;
+
+/** 公共请求方法 */
+- (void)requestMethod:(RequestMethod)requestMethod
+            urlString:(NSString *)urlString
+           parameters:(NSDictionary *)parameters
+   compeletionHandler:(void(^)(id res,NSError *error))compeletionHandler;
 
 @end
--- Bewilder/Bewilder/Common/JSNetworkManager.m
+++ Bewilder/Bewilder/Common/JSNetworkManager.m
@@ -8,6 +8,40 @@
 
 #import "JSNetworkManager.h"
 
+static JSNetworkManager *_instanceType = nil;
+
 @implementation JSNetworkManager
+
++ (instancetype)sharedManager {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _instanceType = [[JSNetworkManager alloc] init];
+    });
+    return _instanceType;
+}
+
+- (void)requestMethod:(RequestMethod)requestMethod
+            urlString:(NSString *)urlString
+           parameters:(NSDictionary *)parameters
+   compeletionHandler:(void(^)(id res,NSError *error))compeletionHandler {
+    
+    if (requestMethod == RequestMethodGet) {
+        [self GET:urlString parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+            
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            compeletionHandler(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            compeletionHandler(nil,error);
+        }];
+    } else {
+        [self POST:urlString parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+            
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            compeletionHandler(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            compeletionHandler(nil,error);
+        }];
+    }
+}
 
 @end
