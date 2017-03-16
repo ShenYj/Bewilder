8d7b7d425f1a71acff42692bf858483306b554c1
ShenYj <shenyj4@51nb.com>
2017-03-16 22:26:32 +0800

    指示条 & 动画效果

--- Bewilder/Bewilder/Classes/Essence/Views/JSEssenceMenuView.m
+++ Bewilder/Bewilder/Classes/Essence/Views/JSEssenceMenuView.m
@@ -14,10 +14,15 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
 
 @interface JSEssenceMenuView ()
 
-
+/** 存放收拾的数组 */
 @property (nonatomic,strong) NSMutableArray *gesturesArr;
 /** 中间变脸,记录当前label */
 @property (nonatomic,weak) JSMenuLabel *selectedLabel;
+/** 指示条 */
+@property (nonatomic,strong) UIView *indicatorView;
+/** 记录指示条约束 */
+@property (nonatomic,strong) MASConstraint *centerX;
+@property (nonatomic,strong) MASConstraint *width;
 
 @end
 
@@ -36,9 +41,7 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
     
     NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
     CGFloat labelWidth = SCREEN_WIDTH / titles.count;
-    __weak typeof(self)weakSelf = self;
     [titles enumerateObjectsUsingBlock:^(NSString *title, NSUInteger idx, BOOL * _Nonnull stop) {
-        __strong typeof(self) self = weakSelf;
         JSMenuLabel *label = [[JSMenuLabel alloc] init];
         label.text = title;
         label.tag = kFLagValue + idx;
@@ -49,16 +52,19 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGesture:)];
         [label addGestureRecognizer:tap];
         [self.gesturesArr addObject:tap];
-        
-        if (idx == 0) {
-            label.scale = 1;
-            self.selectedLabel = label;
-        }
     }];
 
+    [self addSubview:self.indicatorView];
+    [self.indicatorView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(self);
+        make.height.mas_equalTo(1);
+    }];
     [self mas_makeConstraints:^(MASConstraintMaker *make) {
         make.height.mas_equalTo(kMenuViewHeight);
     }];
+    
+    // 首次默认选中
+    [self tapGesture:self.gesturesArr.firstObject];
 }
 
 
@@ -74,6 +80,20 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
         return;
     }
     
+    // 设置指示条
+    [self.centerX uninstall];
+    [self.width uninstall];
+    CGRect bounds = [label.text boundingRectWithSize:CGSizeMake(label.bounds.size.width, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:14]} context:nil];
+    [self.indicatorView mas_updateConstraints:^(MASConstraintMaker *make) {
+        self.centerX = make.centerX.mas_equalTo(label);
+        self.width = make.width.mas_equalTo(bounds.size.width);
+    }];
+    [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.4 initialSpringVelocity:1.0 options:UIViewAnimationOptionLayoutSubviews animations:^{
+        [self layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        
+    }];
+    
     // 获取label中存放的tag值(即索引)
     NSInteger idx = label.tag - kFLagValue;
     
@@ -85,6 +105,7 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
         [self.delegate essenceMenuiew:self index:idx];
     }
     
+    // 请求数据
 }
 
 
@@ -104,6 +125,14 @@ static NSInteger const kFLagValue = 1100;             // tag值的中间变量
         _gesturesArr = [NSMutableArray arrayWithCapacity:5];
     }
     return _gesturesArr;
+}
+
+- (UIView *)indicatorView {
+    if (!_indicatorView) {
+        _indicatorView = [[UIView alloc] init];
+        _indicatorView.backgroundColor = [UIColor redColor];
+    }
+    return _indicatorView;
 }
 
 @end
